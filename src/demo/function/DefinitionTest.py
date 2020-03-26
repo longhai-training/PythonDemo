84293cfde9e8962fabd0ff740568de8b36fb8fcd
longhai-training <378208215@qq.com>
2020-03-26 15:34:55 +0800

    version update

---
+++ src/demo/function/DefinitionTest.py
@@ -0,0 +1,26 @@
+def greet_user(uname):
+    print("Hello "+uname)
+
+def describe_pet(animal_type, pet_name):
+    print("\nI have a " + animal_type + ".")
+    print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+
+# 函数显示宠物的信息
+def describe_pet(animal_type, pet_name):
+    print("\nI have a " + animal_type + ".")
+    print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+# 函数显示宠物的信息 设置默认值为pet_name='willie'; 这种参数声明必须是无默认值跟在有默认值前面
+def describe_pet( animal_type,pet_name='willie'):
+    print("\nI have a " + animal_type + ".")
+    print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+# 调用函数
+greet_user("young man")
+# 传入参数调用函数
+describe_pet('hamster', 'harry')
+# 位置实参,有序实参方式
+describe_pet('hamster', 'harry')
+# 关键字实参,无序实参方式
+describe_pet(animal_type='hamster', pet_name='harry')
+describe_pet(pet_name='harry', animal_type='hamster')
+# 默认值pet_name='willie'生效
+describe_pet(animal_type='dog')
