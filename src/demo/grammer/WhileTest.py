ae518f31e7adae50ee66a6d6541eaf8cc0c06e0c
longhai-training <378208215@qq.com>
2020-03-26 11:11:16 +0800

    version update

---
+++ src/demo/grammer/WhileTest.py
@@ -0,0 +1,33 @@
+current_number = 1
+while current_number <= 5:
+    print(current_number)
+    current_number += 1
+
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+message = ""
+while message != 'quit':
+    message = input(prompt)
+    print(message)
+
+while True:
+    city = input(prompt)
+    if city == 'quit':
+        break
+else:
+    print("I'd love to go to " + city.title() + "!")
+
+
+# 和一个用于存储已验证用户的空列表
+unconfirmed_users = ['alice', 'brian', 'candace']
+confirmed_users = []
+# 验证每个用户，直到没有未验证用户为止
+# 将每个经过验证的列表都移到已验证用户列表中
+while unconfirmed_users:
+    current_user = unconfirmed_users.pop()
+    print("Verifying user: " + current_user.title())
+    confirmed_users.append(current_user)
+# 显示所有已验证的用户
+print("\nThe following users have been confirmed:")
+for confirmed_user in confirmed_users:
+    print(confirmed_user.title())
